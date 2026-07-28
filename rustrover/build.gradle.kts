fe8e61dfe0d02805e895f074dea92e73f6a719e6
failfmi <oscurocalma@gmail.com>
2026-07-28 21:51:50 +0300

    fix: pin until-build to 262.* line

--- rustrover/build.gradle.kts
+++ rustrover/build.gradle.kts
@@ -81,7 +81,7 @@ intellijPlatform {
 
         ideaVersion {
             sinceBuild = providers.gradleProperty("pluginSinceBuild")
-            untilBuild = provider { null }
+            untilBuild = providers.gradleProperty("pluginUntilBuild")
         }
     }
 
